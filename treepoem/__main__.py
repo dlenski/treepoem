4a18c314c5bf8837384163658c6e39d73bba22be
Daniel Lenski <daniel.lenski@finalphasesystems.com>
2018-01-12 03:37:09 -0800

    list supported barcodes

--- treepoem/__main__.py
+++ treepoem/__main__.py
@@ -4,18 +4,25 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 import argparse
 import sys
+from textwrap import fill
 
 from . import generate_barcode
 from .data import barcode_types
 
+supported_barcode_types = ('Supported barcode types are:\n'
+    + fill(', '.join(sorted(barcode_types)), initial_indent='    ', subsequent_indent='    '))
+
 def main():
-    p = argparse.ArgumentParser()
+    p = argparse.ArgumentParser(epilog=supported_barcode_types)
     p.add_argument('-t', '--type', default='qrcode', help='Barcode type (default (%(default)s))')
     p.add_argument('-f', '--fmt', help='Output format (default is based on file extension)')
     p.add_argument('-o', '--output', help='Output file (default is stdout)')
     p.add_argument('data', help='Barcode data')
     p.add_argument('options', nargs='*', type=lambda x: x.split('=', 1)[:2], help='List of BWIPP options (e.g. width=1.5)')
     args = p.parse_args()
+
+    if args.type not in barcode_types:
+        p.error('Barcode type %r is not supported. %s' % (args.type, supported_barcode_types))
 
     if args.output is None:
         args.output = sys.stdout.buffer
