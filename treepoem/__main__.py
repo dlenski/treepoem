286cd11f823d8ae02f4fc63915deec7718035cd0
Daniel Lenski <daniel.lenski@finalphasesystems.com>
2018-01-25 10:39:51 -0800

    cleanup based on PR review

--- treepoem/__main__.py
+++ treepoem/__main__.py
@@ -14,22 +14,29 @@ supported_barcode_types = (
 )
 
 
+if sys.version_info < (3, 0):
+    stdout_binary = sys.stdout
+else:
+    stdout_binary = getattr(sys.stdout, 'buffer')
+
+parser = argparse.ArgumentParser(epilog=supported_barcode_types)
+parser.add_argument('-t', '--type', default='qrcode', help='Barcode type (default %(default)s)')
+parser.add_argument('-f', '--format', help='Output format (default is based on file extension)')
+parser.add_argument('-o', '--output', default=stdout_binary, help='Output file (default is stdout)')
+parser.add_argument('data', help='Barcode data')
+parser.add_argument('options', nargs='*', type=lambda x: x.split('=', 1)[:2], help='List of BWIPP options (e.g. width=1.5)')
+
+
 def main():
-    p = argparse.ArgumentParser(epilog=supported_barcode_types)
-    p.add_argument('-t', '--type', default='qrcode', help='Barcode type (default (%(default)s))')
-    p.add_argument('-f', '--fmt', help='Output format (default is based on file extension)')
-    p.add_argument('-o', '--output', help='Output file (default is stdout)')
-    p.add_argument('data', help='Barcode data')
-    p.add_argument('options', nargs='*', type=lambda x: x.split('=', 1)[:2], help='List of BWIPP options (e.g. width=1.5)')
-    args = p.parse_args()
+    args = parser.parse_args()
 
     if args.type not in barcode_types:
-        p.error('Barcode type %r is not supported. %s' % (args.type, supported_barcode_types))
+        parser.error('Barcode type %r is not supported. %s' % (args.type, supported_barcode_types))
 
-    if args.output is None:
-        args.output = sys.stdout.buffer
-        if args.fmt is None:
-            args.fmt = 'xbm'
+    # PIL needs an explicit format when it doesn't have a filename to guess from
+    if args.output == stdout_binary:
+        if args.format is None:
+            args.format = 'xbm'
 
     image = generate_barcode(args.type, args.data, dict(args.options))
-    image.convert('1').save(args.output, args.fmt)
+    image.convert('1').save(args.output, args.format)
