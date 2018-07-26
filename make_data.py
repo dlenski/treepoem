7def2151fa5bae054b1b40d57d49666ebf0a6eb9
Daniel Lenski <daniel.lenski@finalphasesystems.com>
2018-07-25 20:37:53 -0700

    Make treepoem.barcode_types a dict mapping the BWIPP encoder names to the human-readable names of supported barcode types

--- make_data.py
+++ make_data.py
@@ -20,12 +20,19 @@ def main():
 
 
 def load_barcode_types():
-    barcode_types = set()
+    barcode_types = []
+    barcode_type = barcode_desc = None
     with open(BWIPP_PATH) as fp:
         for line in fp:
             if line.startswith('% --BEGIN ENCODER ') and line.endswith('--\n'):
                 barcode_type = line[:-3].split()[3]
-                barcode_types.add(barcode_type)
+                barcode_desc = None
+            elif line.startswith('% --DESC: '):
+                barcode_desc = line[:-1].split(None, 2)[2]
+            elif line.startswith('% --END ENCODER ') and line.endswith('--\n'):
+                barcode_types.append((barcode_type, barcode_desc))
+                barcode_type = barcode_desc = None
+
     return sorted(barcode_types)
 
 
@@ -36,8 +43,8 @@ def write_out_barcode_types(all_barcode_types):
         fp.write('\n')
         fp.write('# All supported barcode types, extracted from barcode.ps\n')
         fp.write('barcode_types = {\n')
-        for barcode_type in all_barcode_types:
-            fp.write('    {!r},\n'.format(barcode_type))
+        for barcode_type, barcode_desc in all_barcode_types:
+            fp.write('    {!r}: {!r},\n'.format(barcode_type, barcode_desc))
         fp.write('}\n')
 
 
